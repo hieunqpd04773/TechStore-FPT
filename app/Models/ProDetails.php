010fab6a1387bffdc9b1769170ece3953bbf4c18
hieunqpd04773 <hieunqpd04773@fpt.edu.vn>
2022-11-05 11:17:54 +0700

    Hieu Push Model

--- app/Models/ProDetails.php
+++ app/Models/ProDetails.php
@@ -10,11 +10,22 @@ class ProDetails extends Model
     use HasFactory;
     protected $table = 'pro_details';
     public $timestamps = false;
-    // protected $attributes = [
-    //     'image'=>''
-    // ];
+    protected $attributes = [
+        'memory'=>null,
+        'camera'=>null,
+        'display'=>null,
+        'batery'=>null,
+        'os'=>null,
+        'sub_camera'=>null,
+        'cpu'=>null,
+        'ram'=>null,
+        'hight'=>null,
+        'width'=>null,
+        'depth'=>null,
+        'weight'=>null
+    ];
     protected $fillable = [
-        'pro_id','hight','width','weight'
+        'pro_id','memory','camera','display','batery','os','sub_camera','cpu','ram','hight','width','depth','weight'
     ];
 
     public function Products()
